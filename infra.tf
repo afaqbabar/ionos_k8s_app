c14ba7a35ac13847bb1667bc50d403437a00b0a1
Afaq Babar <afaqbabar@gmail.com>
2022-05-27 18:21:26 +0000

    add nodepool

--- infra.tf
+++ infra.tf
@@ -1,5 +1,52 @@
+resource "ionoscloud_datacenter" "dc_01" {
+  name                = var.dc_name
+  location            = var.dc_location
+  description         = var.dc_description
+  sec_auth_protection = false
+}
+
+resource "ionoscloud_lan" "lan_01" {
+
+  datacenter_id = ionoscloud_datacenter.dc_01.id
+  public        = false
+  name          = var.lan_name
+}
+
+
+resource "ionoscloud_ipblock" "ipblock_01" {
+  location = var.dc_location
+  size     = var.ipblock_size
+  name     = var.ipblock_name
+}
+
+
+
 resource "ionoscloud_k8s_cluster" "k8s_cluster_03" {
   name        = var.k8s_name
   k8s_version = var.k8s_ver
   public      = true
+}
+
+resource "ionoscloud_k8s_node_pool" "k8s_node_pool_01" {
+  datacenter_id  = ionoscloud_datacenter.dc_01.id
+  k8s_cluster_id = ionoscloud_k8s_cluster.k8s_cluster_01.id
+  name           = var.nodepool_name
+  k8s_version    = ionoscloud_k8s_cluster.k8s_cluster_01.k8s_version
+  maintenance_window {
+    day_of_the_week = "Sunday"
+    time            = "09:00:00Z"
+  }
+  auto_scaling {
+    min_node_count = 2
+    max_node_count = 3
+  }
+  cpu_family        = "INTEL_SKYLAKE"
+  availability_zone = "AUTO"
+  storage_type      = "SSD"
+  node_count        = 2
+  cores_count       = 4
+  ram_size          = 4096
+  storage_size      = 80
+  public_ips        = [ionoscloud_ipblock.ipblock_01.ips[0], ionoscloud_ipblock.ipblock_01.ips[1]]
+
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,34 @@
+variable "dc_name" {
+  description = "Name of DC"
+  default     = "DC for k8s - 01"
+}
+
+variable "dc_location" {
+  description = "The regional location where VDC will be created"
+  default     = "de/fra"
+}
+
+variable "dc_description" {
+  description = "Description of DC"
+  default     = "DC for k8s - 03"
+}
+
+variable "lan_name" {
+  description = "Name of Private LAN"
+  default     = "lan_01"
+}
+
+
+variable "ipblock_name" {
+  description = "Name of IP block"
+  default     = "ipblock_01"
+}
+
+variable "ipblock_size" {
+  description = "Size of IP block"
+  default     = 6
+}
+
 variable "k8s_name" {
   description = "Name of k8s cluster"
   default     = "k8s_cluster_03"
@@ -6,4 +37,9 @@ variable "k8s_name" {
 variable "k8s_ver" {
   description = "K8s Version"
   default     = "1.22.8"
+}
+
+variable "nodepool_name" {
+  description = "Name of node pool"
+  default     = "k8s_nodepool_01"
 }
