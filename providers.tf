ca199f8b696b9d482d3685bf5d826c6e7f8f4abd
Afaq Babar <afaqbabar@gmail.com>
2022-05-27 17:56:53 +0000

    providers.tf

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+
+  required_providers {
+    ionoscloud = {
+      source  = "ionos-cloud/ionoscloud"
+      version = "= 6.2.1"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.6.0"
+    }
+
+
+  }
+
+  cloud {
+    organization = "ionos"
+
+    workspaces {
+      name = "ionos-k8s-app"
+    }
+  }
+}
+
