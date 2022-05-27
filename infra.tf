72f7a8ab9e4f2862945670d355a5cc6c05d49b5d
Afaq Babar <afaqbabar@gmail.com>
2022-05-27 18:24:49 +0000

    corrected reference to resource

--- infra.tf
+++ infra.tf
@@ -29,9 +29,9 @@ resource "ionoscloud_k8s_cluster" "k8s_cluster_03" {
 
 resource "ionoscloud_k8s_node_pool" "k8s_node_pool_01" {
   datacenter_id  = ionoscloud_datacenter.dc_01.id
-  k8s_cluster_id = ionoscloud_k8s_cluster.k8s_cluster_01.id
+  k8s_cluster_id = ionoscloud_k8s_cluster.k8s_cluster_03.id
   name           = var.nodepool_name
-  k8s_version    = ionoscloud_k8s_cluster.k8s_cluster_01.k8s_version
+  k8s_version    = ionoscloud_k8s_cluster.k8s_cluster_03.k8s_version
   maintenance_window {
     day_of_the_week = "Sunday"
     time            = "09:00:00Z"
